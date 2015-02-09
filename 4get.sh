778bc243e7bcf34b5cca66e500ae882b1ae2a9fb
Mees Brinkhuis <MeesGamez@gmail.com>
2015-02-09 23:01:06 +0100

    added .update for future auto updating

--- 4get.sh
+++ 4get.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 #Default directory
-DEFDIR=~Pictures/4chan
+DEFDIR=~/Pictures/4chan
 
-DIR=$DEFDIR/$(echo $1 | awk -F/ '{ print $4"-"$6 }' | awk -F# '{ print $1 }')
+URL=$(echo ${@: -1})
+DIR=$DEFDIR/$(echo $URL | awk -F/ '{ print $4"-"$6 }' | awk -F# '{ print $1 }')
 
-while getopts dP:ah OPT; do
+while getopts dP:a:h OPT; do
     case "$OPT" in
         h)
             echo "Usage: 4get [-dh] [-P <path>] <thread link>"
             echo "default path: $DEFDIR/"
             echo "-d)        empty default path"
-            echo "-a)        add to .update file"
+            echo "-a <arg>)  add to .update file"
+            echo "-u)        update all pages in .update"
             echo "-h)        display this help"
             echo "-P <arg>)  enter required path"
             exit 0
@@ -20,12 +22,11 @@ while getopts dP:ah OPT; do
             rm -rf $DEFDIR/*
             exit 0
             ;;
-        a)
-            rm -rf $DEFDIR/4chan/*
-            exit 0
-            ;;
         P)
             DIR=$OPTARG
+            ;;
+        a)
+            echo $OPTARG\@$DIR >> $DEFDIR/.update
             ;;
         \?)
             echo "-h for help"
@@ -36,16 +37,13 @@ done
 
 shift $((OPTIND-1))
 
-if [ $# -eq 0 ]; then
-    echo "-h for help"
-    exit 1
-fi
+echo $URL
 
 if [ ! -d $DIR ]; then
     mkdir $DIR
 fi
 
-for i in $(curl -s $1 | sed s/href/\\n/g | grep -o "i.4cdn.org.*\"" | awk -F\" '{print $1}' | uniq); do
+for i in $(curl -s $URL | sed s/href/\\n/g | grep -o "i.4cdn.org.*\"" | awk -F\" '{ print $1 }' | uniq); do
     if [ ! -e $DIR/$(echo $i | awk -F/ '{print $3}') ]; then
         echo $(echo $i | awk -F/ '{print $3}')
 	wget -qP $DIR $i
