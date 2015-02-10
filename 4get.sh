30969fa71159d067ff066076b74ae23044c8fcae
Mees Brinkhuis <MeesGamez@gmail.com>
2015-02-10 16:58:30 +0100

    updating works but needs LOADS of cleanup

--- 4get.sh
+++ 4get.sh
@@ -6,7 +6,7 @@ DEFDIR=~/Pictures/4chan
 URL=$(echo ${@: -1})
 DIR=$DEFDIR/$(echo $URL | awk -F/ '{ print $4"-"$6 }' | awk -F# '{ print $1 }')
 
-while getopts dP:a:h OPT; do
+while getopts dP:a:uh OPT; do
     case "$OPT" in
         h)
             echo "Usage: 4get [-dh] [-P <path>] <thread link>"
@@ -28,6 +28,18 @@ while getopts dP:a:h OPT; do
         a)
             echo $OPTARG\@$DIR >> $DEFDIR/.update
             ;;
+        u)
+			for j in $(seq 1 $(wc -l $DEFDIR/.update | awk -F" " '{ print $1 }')); do
+			echo $(awk -F@ '{ print $1 }' $DEFDIR/.update | sed -n "$j"p)
+				for i in $(curl -s $(awk -F@ '{ print $1 }' $DEFDIR/.update | sed -n "$j"p) | sed s/href/\\n/g | grep -o "i.4cdn.org.*\"" | awk -F\" '{ print $1 }' | uniq); do
+					if [ ! -e $(awk -F@ '{ print $2 }' $DEFDIR/.update | sed -n "$j"p)/$(echo $i | awk -F/ '{print $3}') ]; then
+						echo $(echo $i | awk -F/ '{print $3}')
+						wget -qP $(awk -F@ '{ print $2 }' $DEFDIR/.update | sed -n "$j"p) $i
+					fi
+				done
+			done
+			exit 0
+			;;
         \?)
             echo "-h for help"
             exit 1
@@ -36,8 +48,6 @@ while getopts dP:a:h OPT; do
 done
 
 shift $((OPTIND-1))
-
-echo $URL
 
 if [ ! -d $DIR ]; then
     mkdir $DIR
